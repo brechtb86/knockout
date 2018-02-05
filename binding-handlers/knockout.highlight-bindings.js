c0725cbd590b3af4c4210ab907c2726563df52a1
Brecht Baekelandt <brecht.baekelandt@outlook.com>
2018-02-05 17:09:17 +0100

    First binding handler.

---
+++ binding-handlers/knockout.highlight-bindings.js
@@ -0,0 +1,76 @@
+﻿(function () {
+    "use strict";
+
+    // locals
+    var unwrap = ko.utils.unwrapObservable;
+
+    /**
+    * initiate        
+    *
+    * @param {element} element
+    * @param {object} value
+    * @param {object} bindings
+    * @api public
+    */
+    function init(element, value, bindings) {
+        var $el = $(element);
+        var modelValue = unwrap(value());
+
+        var text = modelValue.text || "";
+        var keywords = modelValue.keywords || [0];
+
+        var highLightedText = highlight(text, keywords);
+
+        $el.html(highLightedText);
+    }
+
+    /**
+    * update
+    *
+    * @param {element} element
+    * @param {object} value
+    * @param {object} bindings
+    * @api public
+    */
+    function update(element, value, bindings) {
+        var $el = $(element);
+        var modelValue = unwrap(value());
+
+        var text = modelValue.text || "";
+        var keywords = modelValue.keywords || [0];
+
+        var highLightedText = highlight(text, keywords);
+
+        $el.html(highLightedText);
+    }
+
+    /**
+    * highlight
+    *
+    * @param {object} text
+    * @param {object} keywords
+    * @api private
+    */
+    function highlight(text, keywords) {
+        var newText = text;
+
+        if (text && keywords && keywords.length > 0) {
+            keywords.forEach(function (keyword) {
+                if (keyword) {
+                    newText = (!newText.startsWith("<") ? "<p>" + newText + "</p>" : newText).replace(/(>[^<]+)/igm,
+                        function (result) {
+                            return result.replace(new RegExp("(" + keyword + ")", "igm"),
+                                "<span class='high-lighted'>$1</span>");
+                        });
+                }
+            });
+        }
+
+        return newText;
+    }
+
+    ko.bindingHandlers.highlight = {
+        init: init,
+        update: update
+    }
+})();
